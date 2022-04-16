18c6dc3606c950d78b380f96f3ea13907195c2ba
cuddleofdeath <cuddleofdeath@gmail.com>
2022-04-16 15:00:00 -0500

    customized products

--- lib/products.ts
+++ lib/products.ts
@@ -1,18 +1,27 @@
 export const products = [
   {
-    id: 'box-of-cookies',
+    id: 'cupcake-single',
+    name: 'Sample',
+    description:
+      'A tiny box containing 1 cupcake to sample. Your choice of flavor!',
+    unitName: 'sample', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.02,
+    priceUsd: 2,
+  },
+  {
+    id: 'box-of-cupcakes',
     name: 'Box',
-    description: 'A delicious box of handmade cookies',
+    description: 'A delicious box with various flavors of cupcakes',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
+    id: 'basket-of-cupcakes',
     name: 'Basket',
-    description: 'A large basket of handmade cookies',
+    description: 'A large basket full of an assortment of cupcakes',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
-  }
+  },
 ]
