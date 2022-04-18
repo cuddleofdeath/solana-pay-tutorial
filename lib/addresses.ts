d7003c39d7622755efc24a0fbc0425ebaeda4b54
cuddleofdeath <cuddleofdeath@gmail.com>
2022-04-17 19:52:30 -0500

    added usdc address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -4,3 +4,7 @@ import { PublicKey } from '@solana/web3.js'
 export const shopAddress = new PublicKey(
   'DJ5KyGyi1JZwhW3bWXR1m2fgSiaKTnaMWCdc7mohLrDj'
 )
+
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
